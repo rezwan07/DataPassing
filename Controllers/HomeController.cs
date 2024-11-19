711a17d6eb15d81d206d6f0abf5a5661f4112573
Rezwan Ahmed <rez1.ahmed007@gmail.com>
2024-11-19 21:33:01 +0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using DataPassing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataPassing.Controllers
+{
+    public class HomeController : Controller
+    {
+         //GET: Home
+        //public ActionResult Index()
+        //{
+        //    //Person person = new Person();
+        //    //person.Name="rez2";
+        //    //person.Salary = 120000;
+
+        //    List<Person> people = new List<Person>()
+        //    {
+        //        new Person() {Name="rez1" , Salary= 1111111},
+        //        new Person() {Name="rez2" , Salary= 2222222},
+        //    };
+
+        //    //TempData["Name"] = "rez1";
+        //    return View(people);
+        //}
+
+        public ActionResult Index()
+        {
+            if(Request.HttpMethod == "get")
+            {
+                return View();
+            }
+            return Content(Request["name"]);
+            //return View();
+        }
+
+        //public ActionResult Another()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataPassing.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+    }
+}
